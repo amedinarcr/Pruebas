c402713ca6bb778bb9e99b7bd2ae42afeb431326
Alejandro Medina <amedina@rcr.com.mx>
2021-08-10 17:43:44 -0700

    otros cambios

--- prueba.js
+++ prueba.js
@@ -1,5 +1,10 @@
 //Function to add 2 numbers 
-func sum2numbers(number1, number2)
+function sum2numbers(number1, number2)
 {
     return number1+number2;
+}
+
+// Validate email
+function validateEmail(input){
+    return true;
 }
